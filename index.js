e0ccce758106ef4a729bf653326313bad4722f61
dmitri9149 <dmitri.as.name@gmail.com>
2020-12-29 21:58:32 +0200

    some bugs fixed

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const http = require('http')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
 
-const PORT = 3003
-app.listen(config.PORT, () => {
+const server = http.createServer(app)
+
+server.listen(config.PORT, () => {
   logger.info(`Server running on port ${PORT}`)
 })
