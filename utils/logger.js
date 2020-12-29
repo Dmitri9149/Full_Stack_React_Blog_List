c8c9f277f3f6572b2b26dbfe8659d20faefc451f
dmitri9149 <dmitri.as.name@gmail.com>
2020-12-29 18:43:55 +0200

    new trial

--- utils/logger.js
+++ utils/logger.js
@@ -1,7 +1,11 @@
-const info = () => {
-    console.log('connecting.to', url)
-}
-
-module.exports = {
-    info
+const info = (...params) => {
+    console.log(...params)
+  }
+  
+  const error = (...params) => {
+    console.error(...params)
+  }
+  
+  module.exports = {
+    info, error
   }
