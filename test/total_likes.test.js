addd4b40d9dd5624eac576f4a76735ed32709cfe
dmitri9149 <dmitri.as.name@gmail.com>
2020-12-30 14:14:38 +0200

    bug fixed

--- test/total_likes.test.js
+++ test/total_likes.test.js
@@ -63,7 +63,7 @@ describe('total likes', () => {
   
     test('when list has only one blog, equals the likes of that', () => {
       const result = listHelper.totalLikes(listWithOneBlog)
-      expect(result).toBe(36)
+      expect(result).toBe(5)
     })
 
     test('of empty list is zero', () => {
@@ -73,6 +73,6 @@ describe('total likes', () => {
     
     test('of a bigger list is calculated right', () => {
       const result = listHelper.totalLikes(blogs)
-      expect(result).toBe(5)
+      expect(result).toBe(36)
     })
   })
