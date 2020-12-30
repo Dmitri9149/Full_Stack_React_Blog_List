eb32eddfb35e76acd1bea44f8ca571f9544def73
dmitri9149 <dmitri.as.name@gmail.com>
2020-12-30 14:13:32 +0200

    test for list on many blogs

--- test/total_likes.test.js
+++ test/total_likes.test.js
@@ -63,11 +63,16 @@ describe('total likes', () => {
   
     test('when list has only one blog, equals the likes of that', () => {
       const result = listHelper.totalLikes(listWithOneBlog)
-      expect(result).toBe(5)
+      expect(result).toBe(36)
     })
 
     test('of empty list is zero', () => {
       const result = listHelper.totalLikes([])
       expect(result).toBe(0)
-    })    
+    }) 
+    
+    test('of a bigger list is calculated right', () => {
+      const result = listHelper.totalLikes(blogs)
+      expect(result).toBe(5)
+    })
   })
