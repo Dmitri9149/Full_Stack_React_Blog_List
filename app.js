a1b5361222f0b4f96a4530e00601c5821a7ec727
dmitri9149 <dmitri.as.name@gmail.com>
2020-12-30 10:11:57 +0200

    ./utils/config stated first

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+/*const middleware = require('/utils/middleware')*/
 const mongoose = require('mongoose')
-require('dotenv').config()
 const logger = require('./utils/logger')
-const config = require('./utils/config')
 
 logger.info('connecting.to', config.MONGODB_URI)
 
@@ -14,5 +15,8 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
 
 app.use(cors())
 app.use(express.json())
+/*app.use(middleware.requestLogger)*/
+
+app.use('/api/blogs', blogsRouter)
 
 module.exports = app
