01bc7f263ec8019913074cfa790d6cd2349a6fd5
dmitri9149 <dmitri.as.name@gmail.com>
2020-12-31 11:55:02 +0200

    mostBlogs

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -27,9 +27,36 @@ const favoriteBlog = array => {
     : array.reduce(reducer,{title:array[0].title, author:array[0].author, likes:array[0].likes})
 }
 
+const groupByAuthorCollection = blogs => {
+  const collectionA = _.groupBy(blogs, function(o) {return o.author})
+  const collectionB = _.map(collectionA, function(value, key){ return [ key, value.length, totalLikes(value) ]  } )
+  return collectionB
+}
+
+const mostBlogs = blogs => {
+  const collection = groupByAuthorCollection(blogs)
+  const reducer = (sum, blog) => {
+    if(sum[1] < blog[1]) {
+      return blog
+    } else {
+      return sum
+    }
+  }
+
+  return collection.length === 0
+    ? {}
+    : {
+      author:collection.reduce(reducer, collection[0])[0],
+      blogs:collection.reduce(reducer, collection[0])[1]
+    }
+
+}
+
+
 
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
