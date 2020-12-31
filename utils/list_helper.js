ad40d594868c6b7cad7fef7d5886b2ef549be8b8
dmitri9149 <dmitri.as.name@gmail.com>
2020-12-31 10:51:45 +0200

    bug fixed

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -14,16 +14,17 @@ const totalLikes = array => {
 }
 
 const favoriteBlog = array => {
+
   const reducer = (acc, current) => {
     return current.likes > acc.likes
       ? { title:current.title, author:current.author, likes:current.likes }
-      : { titile:acc.title, author:acc.author, likes:acc.likes }
+      : { title:acc.title, author:acc.author, likes:acc.likes }
     
   }
 
   return array.length === 0
     ? {}
-    : array.reduce(reducer)
+    : array.reduce(reducer,{title:array[0].title, author:array[0].author, likes:array[0].likes})
 }
 
 
