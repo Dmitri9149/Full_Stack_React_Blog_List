f3c855f656d42b024687c45caabc693d2d7426f3
dmitri9149 <dmitri.as.name@gmail.com>
2021-01-05 15:40:38 +0200

    delete controller

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -30,6 +30,17 @@ try {
   next(exeption)
 }
 
+blogsRouter.delete('/:id', async (request, response, next) => {
+  try {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+  } catch (exception) {
+    next(exception)
+  }
 })
+
+})
+
+
 
 module.exports = blogsRouter
