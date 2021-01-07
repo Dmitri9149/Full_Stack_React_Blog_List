d483c150022953a49ae8207fbd0c57c2833071c1
dmitri9149 <dmitri.as.name@gmail.com>
2021-01-07 22:57:07 +0200

    some development

--- app.js
+++ app.js
@@ -11,8 +11,6 @@ const logger = require('./utils/logger')
 
 logger.info('connecting.to', config.MONGODB_URI)
 
-
-/* const mongoUrl = 'mongodb+srv://blog_list:1961dm20XX@cluster0.2m41j.mongodb.net/blog_list?retryWrites=true&w=majority' */
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
 app.use(cors())
