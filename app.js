d102cad73cff5dafcb188a6c306c691319752fe9
dmitri9149 <dmitri.as.name@gmail.com>
2021-01-07 17:56:10 +0200

    error handling by middleware is added

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
-const middleware = require('/utils/middleware')
+const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 
@@ -21,5 +21,8 @@ app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
