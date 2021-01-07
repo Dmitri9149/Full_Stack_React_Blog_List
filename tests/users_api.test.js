5a3e639cabef67ba8ad45df7b66fe959c7a6251c
dmitri9149 <dmitri.as.name@gmail.com>
2021-01-07 13:10:30 +0200

    bug fixed

--- tests/users_api.test.js
+++ tests/users_api.test.js
@@ -58,7 +58,7 @@ describe('when there is initially one user at db', () => {
 
     expect(result.body.error).toContain('`username` to be unique')
 
-    const usersAtEnd = await helper.usersInDb()
+    const usersAtEnd = await listHelper.usersInDb()
     expect(usersAtEnd).toHaveLength(usersAtStart.length)
   })
 
