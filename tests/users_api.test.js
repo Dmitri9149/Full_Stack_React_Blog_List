42681df8138809e2b63c130e8096af62816c744f
dmitri9149 <dmitri.as.name@gmail.com>
2021-01-07 13:06:56 +0200

    name changed

--- tests/users_api.test.js
+++ tests/users_api.test.js
@@ -42,7 +42,7 @@ describe('when there is initially one user at db', () => {
   })
 
   test('creation fails with proper statuscode and message if username already taken', async () => {
-    const usersAtStart = await helper.usersInDb()
+    const usersAtStart = await listHelper.usersInDb()
 
     const newUser = {
       username: 'root',
