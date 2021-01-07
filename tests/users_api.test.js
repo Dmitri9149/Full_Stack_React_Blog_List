a8aa4fae7dd10589af7d1da1ee2383665df1dd69
dmitri9149 <dmitri.as.name@gmail.com>
2021-01-07 15:28:17 +0200

    test for uniq username

--- tests/users_api.test.js
+++ tests/users_api.test.js
@@ -39,6 +39,31 @@ describe('when there is initially one user at db', () => {
   })
 })
 
+describe('when there is initially one user at db', () => {
+
+  test('creation fails with proper statuscode and message if username already taken', async () => {
+    const usersAtStart = await listHelper.usersInDb()
+
+    const newUser = {
+      username: 'root',
+      name: 'Superuser',
+      password: 'salainen',
+    }
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    expect(result.body.error).toContain('`username` to be unique')
+
+    const usersAtEnd = await listHelper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length)
+  })
+
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
