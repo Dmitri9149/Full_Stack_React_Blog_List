165f178752c2e5ea211429e804c173bc5875cd27
dmitri9149 <dmitri.as.name@gmail.com>
2021-01-12 16:11:30 +0200

    delete method

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -55,4 +55,26 @@ blogsRouter.put('/:id', async (request, response, next) => {
   const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
   response.json(updatedBlog.toJSON())
 })
+
+blogsRouter.delete('/:id', async (request, response, next) => {
+
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+
+  const userid = decodedToken.id
+
+  const blog = await Blog.findById(request.params.id)
+
+  if ( blog.user.toString() === userid.toString() ) {
+
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+  } else {
+    return response.status(400).json({ error: 'token does not correspond to the blog creator' })
+  }
+})
+
+
 module.exports = blogsRouter
